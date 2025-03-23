31076a95238152b0d4e7c250361edad191d36ed2
wangjw.dev <wangjw.dev@gmail.com>
2025-03-23 15:35:40 +0900

    category stock

--- mall-admin/src/api/pms/brand/index.ts
+++ mall-admin/src/api/pms/brand/index.ts
@@ -2,81 +2,89 @@ import { BrandForm, Brand, BrandPageResult, BrandQuery } from "./types";
 import request from "@/utils/request";
 import { AxiosPromise } from "axios";
 
-/**
- * 获取品牌分页列表
- *
- * @param queryParams
- */
-export function getBrandPage(
-  queryParams: BrandQuery
-): AxiosPromise<BrandPageResult> {
-  return request({
-    url: "/mall-pms/api/v1/brands/page",
-    method: "get",
-    params: queryParams,
-  });
-}
+const PMS_BRAND_BASE_URL = "/api/v1/brands";
 
-/**
- * 获取品牌列表
- *
- * @param queryParams
- */
-export function getBrandList(queryParams?: BrandQuery): AxiosPromise<Brand[]> {
-  return request({
-    url: "/mall-pms/api/v1/brands",
-    method: "get",
-    params: queryParams,
-  });
-}
+const BrandAPI = {
+  /**
+   * Retrieves a paginated list of brands.
+   *
+   * This function sends a GET request to fetch a paginated list of brands based on the provided query parameters.
+   *
+   * @param queryParams - An object of type BrandQuery containing the query parameters for pagination and filtering.
+   * @returns A promise that resolves to an AxiosResponse containing a BrandPageResult object.
+   *          The BrandPageResult likely includes the list of brands and pagination information.
+   */
+  getBrandPage(queryParams: BrandQuery): AxiosPromise<BrandPageResult> {
+    return request({
+      url: `${PMS_BRAND_BASE_URL}/page`,
+      method: "get",
+      params: queryParams,
+    });
+  },
 
-/**
- * 获取品牌详情
- *
- * @param id
- */
-export function getBrandFormDetail(id: number): AxiosPromise<BrandForm> {
-  return request({
-    url: "/mall-pms/api/v1/brands/" + id,
-    method: "get",
-  });
-}
+  /**
+   * 获取品牌列表
+   *
+   * @param queryParams
+   */
+  getBrandList(queryParams?: BrandQuery): AxiosPromise<Brand[]> {
+    return request({
+      url: PMS_BRAND_BASE_URL,
+      method: "get",
+      params: queryParams,
+    });
+  },
 
-/**
- * 添加品牌
- *
- * @param data
- */
-export function addBrand(data: BrandForm) {
-  return request({
-    url: "/mall-pms/api/v1/brands",
-    method: "post",
-    data: data,
-  });
-}
+  /**
+   * 获取品牌详情
+   *
+   * @param id
+   */
+  getBrandFormDetail(id: number): AxiosPromise<BrandForm> {
+    return request({
+      url: `${PMS_BRAND_BASE_URL}/${id}`,
+      method: "get",
+    });
+  },
 
-/**
- * 修改品牌
- *
- * @param id
- * @param data
- */
-export function updateBrand(id: number, data: BrandForm) {
-  return request({
-    url: "/mall-pms/api/v1/brands/" + id,
-    method: "put",
-    data: data,
-  });
-}
+  /**
+   * 添加品牌
+   *
+   * @param data
+   */
+  addBrand(data: BrandForm) {
+    return request({
+      url: PMS_BRAND_BASE_URL,
+      method: "post",
+      data: data,
+    });
+  },
 
-/**
- * 删除品牌
- *
- * @param ids
- */
-export function deleteBrands(ids: string) {
-  return request({
-    url: "/mall-pms/api/v1/brands/" + ids,
-    method: "delete",
-  });
-}
+  /**
+   * 修改品牌
+   *
+   * @param id
+   * @param data
+   */
+  updateBrand(id: number, data: BrandForm) {
+    return request({
+      url: `${PMS_BRAND_BASE_URL}/${id}`,
+      method: "put",
+      data: data,
+    });
+  },
+
+  /**
+   * 删除品牌
+   *
+   * @param ids
+   */
+  deleteBrands(ids: string) {
+    return request({
+      url: `${PMS_BRAND_BASE_URL}/${ids}`,
+      method: "delete",
+    });
+  },
+};
+
+export default BrandAPI;
---
+++ mall-boot/src/main/java/com/youlai/boot/modules/product/service/BrandService.java
@@ -0,0 +1,8 @@
+package com.youlai.boot.modules.product.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.youlai.boot.modules.product.model.entity.PmsBrand;
+
+public interface BrandService extends IService<PmsBrand> {
+
+}

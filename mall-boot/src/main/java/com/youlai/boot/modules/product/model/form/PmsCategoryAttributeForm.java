179bd35ac3efbf02bfc8786dea7313f20aabd673
wangjw.dev <wangjw.dev@gmail.com>
2025-03-23 21:53:32 +0900

    detail→商品一覧画面遷移時, 商品列表页面没刷新

--- mall-boot/src/main/java/com/youlai/boot/modules/product/model/form/PmsCategoryAttributeForm.java
+++ mall-boot/src/main/java/com/youlai/boot/modules/product/model/form/PmsCategoryAttributeForm.java
@@ -1,5 +1,6 @@
 package com.youlai.boot.modules.product.model.form;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;

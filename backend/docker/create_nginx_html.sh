5144e929912b2cadd69aa5ab0b80b60184b6b7e7
jason.w <jasonw.lab@gmail.com>
2025-05-12 22:34:32 +0900

    photo-gallary

--- backend/docker/create_nginx_html.sh
+++ backend/docker/create_nginx_html.sh
@@ -7,6 +7,7 @@ mkdir -p /mydata/nginx/html/gallery
 mkdir -p /mydata/nginx/html/weather
 mkdir -p /mydata/nginx/html/meal
 mkdir -p /mydata/nginx/html/photo-gallary
+mkdir -p /mydata/nginx/html/vuepress
 
 
 echo "All required directories have been created and files have been copied."
